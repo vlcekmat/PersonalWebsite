1a162cbffc61bc4746b6a892f2a521f26b2333aa
Matej Vlcek <vlcekmat@seznam.cz>
2026-02-22 20:56:55 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>MarlaSite</h1>
+  <p>
+    Welcome to my personal website! My name's Marladian and I am a software engineering student who likes building websites the old way without AI.
+  </p>
+</template>
+
+<style scoped></style>
