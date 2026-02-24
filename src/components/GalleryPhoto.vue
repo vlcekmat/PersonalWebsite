8d8e4486930018db99774b52244a9a7bd5151602
Matej Vlcek <vlcekma>
2026-02-24 21:10:33 +0100

    css adjustments

--- src/components/GalleryPhoto.vue
+++ src/components/GalleryPhoto.vue
@@ -4,14 +4,18 @@
         <div class="info-box">
             <h3>{{ props.title }}</h3>
             <dl>
-                <dt>ISO</dt>
-                <dd>{{ props.iso }}</dd>
-
-                <dt>Shutter Speed</dt>
-                <dd>{{ props.ss }}</dd>
-
-                <dt>Aperature</dt>
-                <dd>{{ props.ape }}</dd>
+                <div>
+                    <dt>ISO</dt>
+                    <dd>{{ props.iso }}</dd>
+                </div>
+                <div>
+                    <dt>Shutter</dt>
+                    <dd>{{ props.ss }}</dd>
+                </div>
+                <div>
+                    <dt>Aperture</dt>
+                    <dd>{{ props.ape }}</dd>
+                </div>
             </dl>
         </div>
     </div>
